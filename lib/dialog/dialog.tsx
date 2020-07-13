c265fe6fed64a83fef3b070c0cf5715a99be0f1b
Promise-Devotion <sijun0224@msn.com>
2020-07-13 23:19:32 +0800

    dialog更新

--- lib/dialog/dialog.tsx
+++ lib/dialog/dialog.tsx
@@ -1,11 +1,28 @@
-import React from "react";
+import React, { Fragment } from "react";
+
+import { scopedClassMaker } from '../helpers/classes';
 
 interface Props {
   visible: boolean;
 }
 
+
+const scopedClass = scopedClassMaker('fui-dialog');
+const sc = scopedClass;
 const Dialog: React.FunctionComponent<Props> = (props) => {
-  return props.visible ? <div>dia</div> : null;
+  return (
+    <Fragment>
+      <div className={sc('mask')}></div>
+      <div className={sc('')}>
+        <div className={sc('close')}></div>
+        <header className={sc('header')}>提示</header>
+        <main className={sc('main')}>
+          内容
+        </main>
+        <footer className={sc('footer')}></footer>
+      </div>
+    </Fragment>
+  );
 };
 
 export default Dialog;
--- lib/helpers/classes.tsx
+++ lib/helpers/classes.tsx
@@ -3,3 +3,27 @@ const classes = (...names: (string | undefined)[]) => {
 }
 
 export default classes;
+
+interface Options {
+  extra: string | undefined
+}
+
+interface ClassToggles {
+  [K: string]: boolean
+}
+
+function scopedClassMaker(prefix: string) {
+
+  return (name: string | ClassToggles, options?: Options) =>
+    Object
+      .entries(name instanceof Object ? name : {[name]: name})
+      .filter(kv => kv[1] !== false)
+      .map(kv => kv[0])
+      .map(name => [prefix, name]
+        .filter(Boolean)
+        .join('-'))
+      .concat(options && options.extra || [])
+      .join(' ');
+}
+
+export {scopedClassMaker};
