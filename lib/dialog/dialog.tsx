b74d9ba1b0d525770edc56d3052ae30707db9a34
Promise-Devotion <sijun0224@msn.com>
2020-07-23 00:24:05 +0800

    dialog布局样式

--- lib/dialog/dialog.tsx
+++ lib/dialog/dialog.tsx
@@ -1,24 +1,31 @@
 import React, { Fragment } from "react";
 
 import { scopedClassMaker } from "../helpers/classes";
+import './dialog.scss'
+import Icon from '../index';
 
 interface Props {
   visible: boolean;
 }
 
-const scopedClass = scopedClassMaker("fui-dialog");
+const scopedClass = scopedClassMaker("sui-dialog");
 const sc = scopedClass;
 const Dialog: React.FunctionComponent<Props> = (props) => {
   return (
+    props.visible ? 
     <Fragment>
       <div className={sc("mask")}></div>
       <div className={sc("")}>
-        <div className={sc("close")}></div>
+        <div className={sc("close")}>
+          <Icon name="close" />
+        </div>
         <header className={sc("header")}>提示</header>
         <main className={sc("main")}>内容</main>
         <footer className={sc("footer")}></footer>
       </div>
     </Fragment>
+    :
+    null
   );
 };
 
