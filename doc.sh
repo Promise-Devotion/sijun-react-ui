bd187ff359fd83c7e0c309ef1e7186938937246c
Promise-Devotion <sijun0224@msn.com>
2020-08-10 23:07:43 +0800

    update

--- doc.sh
+++ doc.sh
@@ -1,8 +1,8 @@
 #!/bin/env bash
 yarn doc
-git checkout gh-pages
+git checkout publish-pages
 mv -f doc/* ./
 git add .
 git commit -m "update"
-git push origin gh-pages
+git push origin publish-pages
 git checkout master
