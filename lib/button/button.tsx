66cd79c5b49532a7edd1a248a6c68eccb00d1dda
Promise-Devotion <sijun0224@msn.com>
2020-09-10 00:01:38 +0800

    增加button组件

--- lib/button/button.tsx
+++ lib/button/button.tsx
@@ -3,20 +3,23 @@ import classes from "../helpers/classes";
 import "./button.scss";
 
 interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
-  level?: "normal" | "important" | "danger"
+  level?: "normal" | "important" | "danger" | "primary" | "text";
+  size?: "small" | "medium" | "large"
 }
 
 const Button: React.FunctionComponent<Props> = (props) => {
-  const { className, children, level, ...rest } = props;
+  const { className, children, level, size, ...rest } = props;
+  console.log(props)
   return (
-    <button className={classes("jun-button", `jun-button-${level}`, className)} {...rest}>
-      按钮
+    <button className={classes("jun-button", `jun-button-${level}`, `jun-button-${size}`,  className)} {...rest}>
+      {props.children}
     </button>
   );
 };
 
 Button.defaultProps = {
-  level: "normal"
+  level: "normal",
+  size: "medium"
 }
 
 export default Button;
--- lib/button/example/button.example.tsx
+++ lib/button/example/button.example.tsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import React from "react";
+import Button from "../button";
 
 const ButtonExample = () => {
-    return(
-        <div>button</div>
-    );
-}
+  return (
+    <div>
+      <Button level="normal" className="a" size="small">默认</Button>
+      <Button level="primary" className="a" size="medium">默认</Button>
+      <Button level="danger" className="b c" size="medium">
+        提交
+      </Button>
+      <Button level="important" className="d" size="large">成功</Button>
+    </div>
+  );
+};
 
 export default ButtonExample;
