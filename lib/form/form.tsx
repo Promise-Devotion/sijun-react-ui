56d3600618673c7537d7d3ec205b66864ed8142d
Promise-Devotion <sijun0224@msn.com>
2020-09-22 01:13:06 +0800

    form布局

--- lib/form/form.tsx
+++ lib/form/form.tsx
@@ -1,11 +1,16 @@
-import React, { ReactFragment } from "react";
+import React, {ReactFragment} from "react";
 import Input from "../input/input";
-import classes from "../helpers/classes";
+// import classes from "../helpers/classes";
 import "./form.scss";
+
+import {scopedClassMaker} from '../helpers/classes';
+const scopedClass = scopedClassMaker('jun-form')
+const sc = scopedClass
 
 export interface FormValue {
   [k: string]: any;
 }
+
 interface Props {
   value: FormValue;
   fields: Array<{ name: string; label: string; input: { type: string } }>;
@@ -22,28 +27,58 @@ const Form: React.FunctionComponent<Props> = (props) => {
     props.onSubmit(e);
   };
   const onInputChange = (name: string, value: string) => {
-    const newValue = { ...formData, [name]: value };
+    const newValue = {...formData, [name]: value};
     props.onChange(newValue);
   };
   return (
-    <form className={classes("jun-form")} onSubmit={onSubmit}>
-      <table>
-        {props.fields.map((f) => (
-          <tr className={classes("jun-form-row")} key={f.name}>
-            <td>
-              <label>{f.label}</label>
+    <form className={sc("")} onSubmit={onSubmit}>
+      <table className={sc('table')}>
+        <tbody>
+        {props.fields.map(f =>
+          <tr key={f.name} className={sc('tr')}>
+            <td className={sc('td')}>
+              <span className={sc('label')}>
+                {f.label}
+              </span>
             </td>
-            <td>
+            <td className={sc('td')}>
               <Input
                 type={f.input.type}
                 value={formData[f.name]}
                 onChange={(e) => onInputChange(f.name, e.target.value)}
               />
-              <div>{props.errors[f.name]}</div>
+              <div className={sc('error')}>
+                {
+                  props.errors[f.name]
+                }
+              </div>
             </td>
           </tr>
-        ))}
+        )}
+        {/*<tr className={sc('tr')}>*/}
+        {/*  {props.buttons}*/}
+        {/*</tr>*/}
+        </tbody>
       </table>
+      {/* <table>
+        <tbody>
+          {props.fields.map((f) => (
+            <tr className={classes("jun-form-row")} key={f.name}>
+              <td>
+                <label>{f.label}</label>
+              </td>
+              <td>
+                <Input
+                  type={f.input.type}
+                  value={formData[f.name]}
+                  onChange={(e) => onInputChange(f.name, e.target.value)}
+                />
+                <div>{props.errors[f.name]}</div>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table> */}
       <div>{props.buttons}</div>
     </form>
   );
